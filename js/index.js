927e80cc30cd4c79ef9c828ffff4177d92ceb594
Sveta077 <pugach.svetka@gmail.com>
2022-05-12 14:06:03 +0200

    fix: added changes

--- js/index.js
+++ js/index.js
@@ -1,25 +1,25 @@
-var VirtualKeyboard = {
+let VirtualKeyboard = {
   generate: function(target, matrix, language, uppercase = false) {
-    var owner = this;
+    let owner = this;
     
-    for(var i = 0; i < matrix.length; i++) {
-      var position = matrix[i];
+    for(let i = 0; i < matrix.length; i++) {
+      let position = matrix[i];
       
-      var vkel = document.createElement('div');
+      let vkel = document.createElement('div');
       vkel.setAttribute('class', 'virtual-keyboard-row');
       
-      var vkcol = document.createElement('div');
+      let vkcol = document.createElement('div');
       vkcol.setAttribute('class', 'virtual-keyboard-column');
       
-      for (var j = 0; j < position.length; j++) {
-        var button = document.createElement('button');
+      for (let j = 0; j < position.length; j++) {
+        let button = document.createElement('button');
         
         switch(matrix[i][j]) {
           case '+backspace': 
             button.innerHTML = '<i class="fa fa-fw fa-long-arrow-left"></i>';
             button.setAttribute('data-trigger', 'backspace');
             button.setAttribute('title', 'Backspace');
-            var mouseTimerHandler = null;
+            let mouseTimerHandler = null;
             button.addEventListener("mousedown", function(event) {
 
               mouseTimerHandler = setInterval(function(){
@@ -57,14 +57,14 @@ var VirtualKeyboard = {
         button.setAttribute('class', 'virtual-keyboard-button');
         button.addEventListener('click', function () {
           _lastElementFocused.focus();
-          var x = this.getAttribute('data-trigger');
+          x = this.getAttribute('data-trigger');
           if (x != null) {
             switch(x) {
               case 'backspace':
                 _lastElementFocused.value = _lastElementFocused.value.slice(0, -1);
                 break;
               case 'international':
-                var reversed = language === 'en'? 'ru' : 'en';
+                let reversed = language === 'en'? 'ru' : 'en';
                 target.innerHTML = '';
                 owner.generate(target,owner.getMatrix(reversed), reversed);
                 break;
@@ -72,7 +72,7 @@ var VirtualKeyboard = {
                 _lastElementFocused.value = _lastElementFocused.value + ' ';
                 break;
               case 'shift':
-                var u = uppercase === true ? false : true;
+                let u = uppercase === true ? false : true;
                 target.innerHTML = '';
                 owner.generate(target,owner.getMatrix(language), language, u);
                 break;
@@ -90,7 +90,7 @@ var VirtualKeyboard = {
     }
   },
   getMatrix: function(language) {
-    var matrix = {en: [
+    let matrix = {en: [
       ['1','2','3','4','5','6','7','8','9','0','+backspace'],
       ['q','w','e','r','t','y','u','i','o','p','-'],
       ['a','s','d','f','g','h','j','k','l','+'],
@@ -109,17 +109,17 @@ var VirtualKeyboard = {
   init: function(args) {
     if (args != undefined && args != null) {
       if (Object.keys(args).length > 0) {
-        var owner = this;
+        let owner = this;
 
         window._lastElementFocused = null;
 
-        var target = document.getElementById(args['targetId']);
-        var language = args['defaultLanguage'];
-        var elements = document.querySelectorAll(args['inputSelector']);
+        let target = document.getElementById(args['targetId']);
+        let language = args['defaultLanguage'];
+        let elements = document.querySelectorAll(args['inputSelector']);
 
         _lastElementFocused = elements[0];
 
-        for (var i = 0; i < elements.length; i++) {
+        for (let i = 0; i < elements.length; i++) {
           elements[i].addEventListener('focus', function () {
             _lastElementFocused = this;
           });
